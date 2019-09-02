53ae442bdfc7665aa2af455854125ad605d38f9e
Alex Coupe <Alex.Coupe@slis.local>
2019-09-02 11:20:47 +0100

    Task model added

---
+++ Models/Task.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace project_manager.Models 
+{
+    class Task 
+    {
+        public long Id {get; set;}
+
+        [DataType(DataType.Text)]
+        public string Name {get; set;}
+
+        [DataType(DataType.MultilineText)]
+        public string Description {get; set;}
+
+        [DataType(DataType.Text)]
+        public string AssignedTo {get; set;}
+
+        [DataType(DataType.Date)]
+        public DateTime DateAssigned {get; set;} 
+
+        public bool Completed {get; set;}
+        
+         [DataType(DataType.Date)]
+        public DateTime CompletedDate {get; set;}
+    }
+}
