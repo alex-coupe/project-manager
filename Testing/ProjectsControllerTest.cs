bad2336dec9e68c96f97bc33eab8a9d4392e34ed
Alex Coupe <Alex.Coupe@slis.local>
2019-09-02 12:23:17 +0100

    Testing project added

---
+++ Testing/ProjectsControllerTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class ProjectsControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetReturnsAllProjects()
+        {
+            Assert.Pass();
+        }
+    }
+}
