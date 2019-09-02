b2f6585a325f0c1bd41c36abc0d2e5888b699401
Alex Coupe <Alex.Coupe@slis.local>
2019-09-02 13:41:17 +0100

    Project controller now works

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ProjectManager.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjectManager
 {
@@ -25,7 +26,7 @@ namespace ProjectManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddDbContext<ApplicationContext>();
+            services.AddDbContext<ApplicationContext>(options => options.UseInMemoryDatabase("ProjectManager"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
