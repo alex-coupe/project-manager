95728d0c9a2ef19310a549a72798783e48a0f518
Alex Coupe <Alex.Coupe@slis.local>
2019-09-06 15:26:36 +0100

    Centered!

--- client/project-manager/src/Components/Card.js
+++ client/project-manager/src/Components/Card.js
@@ -7,10 +7,10 @@ export default class Card extends Component {
     render() {
         return (
             <div>
-                <div id="overview" className="card" style={{width: 'auto'}}>
+                <div id="overview" className="card d-flex justify-content-between" style={{width: 'auto'}}>
                     <h2 className="card-header text-center"><FontAwesomeIcon icon={faEdit}/> 
                         {this.props.name} <span className="badge badge-dark">{this.props.size}</span></h2>
-                    <div className="card-body" style={{backgroundColor: "#fff6db"}}>
+                    <div className="card-body text-center" style={{backgroundColor: "#fff6db"}}>
                         <div>{this.props.children}</div>
                         <Link className="btn btn-primary text-white" to={this.props.link}>{this.props.buttonText}</Link>
                     </div>
