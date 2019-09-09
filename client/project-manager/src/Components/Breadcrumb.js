a49eab2ebd4e5517a0ba33a56428954962a2c2df
Alex Coupe <Alex.Coupe@slis.local>
2019-09-09 10:46:00 +0100

    Removed react router use

--- client/project-manager/src/Components/Breadcrumb.js
+++ client/project-manager/src/Components/Breadcrumb.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 export default function Breadcrumb(props) {
     return (
         <div>
             <nav aria-label="breadcrumb" className="mt-3">
                 <ol className="breadcrumb">
-                    <li className="breadcrumb-item mx-auto" aria-current="page"><h1><Link to="/">Projects</Link>/ {props.name}</h1></li>
+                    <li className="breadcrumb-item mx-auto" aria-current="page"><h1><a href="/">Projects</a>/ {props.name}</h1></li>
                 </ol>
             </nav>
         </div>
