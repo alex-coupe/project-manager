c71827a8eeb84fb5bc0d472f6dcf5e61803ef226
Alex Coupe <Alex.Coupe@slis.local>
2019-09-09 10:46:13 +0100

    Spacing

--- client/project-manager/src/Components/Card.js
+++ client/project-manager/src/Components/Card.js
@@ -7,7 +7,7 @@ export default class Card extends Component {
     render() {
         return (
             <div>
-                <div id="overview" className="card d-flex justify-content-between" style={{width: 'auto'}}>
+                <div id="overview" className="card mb-3 d-flex justify-content-between" style={{width: 'auto'}}>
                     <h2 className="card-header text-center"><FontAwesomeIcon icon={faEdit}/> 
                         {this.props.name} <span className="badge badge-dark">{this.props.size}</span></h2>
                     <div className="card-body text-center" style={{backgroundColor: "#f7f7f7"}}>
